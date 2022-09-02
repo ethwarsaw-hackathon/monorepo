0e34be4c044024f9b3987f782eff8329db41422f
brage laptop <5640782+2xic@users.noreply.github.com>
2022-09-02 21:34:18 +0200

    added the idriss middleware

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,27 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import {IdrissCrypto} from "idriss-crypto";
 
 var app = new Koa();
 var router = new Router();
 
 router.get('/', (ctx) => {
-  // ctx.router available
   ctx.response.body = 'ok';
 });
+
+router.get('/resolve', async (ctx) => {
+    if (ctx.query.value){
+        const idriss = new IdrissCrypto();
+        const resultEmail = await idriss.resolve("hello@idriss.xyz");
+
+        ctx.response.body = resultEmail;
+        ctx.response.status = 200;
+    } else {
+        ctx.response.body = 'missing value parameters';
+        ctx.response.status = 400;
+    }
+});
+  
 
 app
   .use(router.routes())
